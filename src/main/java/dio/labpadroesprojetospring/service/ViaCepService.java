a8e45637fc78a93ae9fab2f364c39514c43d4934
Julio <juliobrito.tec@gmail.com>
2023-10-22 04:44:28 -0300

    update src/main/java/dio/labpadroesprojetospring/service/ViaCepService.java

--- src/main/java/dio/labpadroesprojetospring/service/ViaCepService.java
+++ src/main/java/dio/labpadroesprojetospring/service/ViaCepService.java
@@ -4,7 +4,6 @@ import dio.labpadroesprojetospring.model.Endereco;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
