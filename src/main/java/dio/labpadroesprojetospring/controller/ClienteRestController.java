9b3b65577d28f8424674d770f9ddd50454ab63dd
Julio <juliobrito.tec@gmail.com>
2023-10-22 04:25:40 -0300

    feat: controller classes

---
+++ src/main/java/dio/labpadroesprojetospring/controller/ClienteRestController.java
@@ -0,0 +1,46 @@
+package dio.labpadroesprojetospring.controller;
+
+import dio.labpadroesprojetospring.model.Cliente;
+import dio.labpadroesprojetospring.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("clientes")
+public class ClienteRestController {
+
+    private final ClienteService clienteService;
+
+    @Autowired
+    public ClienteRestController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<Cliente>> buscarTodos() {
+        return ResponseEntity.ok(clienteService.buscarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(clienteService.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente) {
+        clienteService.inserir(cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+        clienteService.atualizar(id, cliente);
+        return ResponseEntity.ok(cliente);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Cliente> deletar(@PathVariable Long id) {
+        clienteService.deletar(id);
+        return ResponseEntity.ok().build();
+    }
+}
