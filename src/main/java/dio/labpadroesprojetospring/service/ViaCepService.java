4b535012efc9909b0ae66b2a97ed7bc05e5b8d3c
Julio <juliobrito.tec@gmail.com>
2023-10-22 04:25:52 -0300

    feat: service classes

---
+++ src/main/java/dio/labpadroesprojetospring/service/ViaCepService.java
@@ -0,0 +1,14 @@
+package dio.labpadroesprojetospring.service;
+
+import dio.labpadroesprojetospring.model.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+    @GetMapping("{cep}/json")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
---
+++ src/main/java/dio/labpadroesprojetospring/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,65 @@
+package dio.labpadroesprojetospring.service.impl;
+
+import dio.labpadroesprojetospring.model.Cliente;
+import dio.labpadroesprojetospring.model.ClienteRepository;
+import dio.labpadroesprojetospring.model.Endereco;
+import dio.labpadroesprojetospring.model.EnderecoRepository;
+import dio.labpadroesprojetospring.service.ClienteService;
+import dio.labpadroesprojetospring.service.ViaCepService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private ViaCepService viaCepService;
+
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return clienteRepository.findAll();
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return clienteRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+        salvarClienteComCep(cliente);
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+        Optional<Cliente> clienteBd = clienteRepository.findById(id);
+        if (clienteBd.isPresent()) {
+            salvarClienteComCep(cliente);
+        }
+    }
+
+    @Override
+    public void deletar(Long id) {
+        clienteRepository.deleteById(id);
+    }
+
+    private void salvarClienteComCep(Cliente cliente) {
+        // Verificar se o Endereco do Cliente já existe pelo CEP.
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+            // Caso não exista, integrar com o ViaCEP e persistir o retorno.
+            Endereco novoEndereco = viaCepService.consultarCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco);
+        // Inserir Cliente, vinculando o Endereço (novo ou existente).
+        clienteRepository.save(cliente);
+    }
+}
